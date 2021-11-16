1944712e6a6ce68434f836cce57ee48c9215fdb0
OLIVEIRA <OLIVEIRA@DESKTOP-HBKMCI1>
2021-11-16 18:15:48 -0300

    Parte 3 adicionado. ADO completa

--- ado5Grupo8/src/main/java/exercicios/parte3.java
+++ ado5Grupo8/src/main/java/exercicios/parte3.java
@@ -5,6 +5,9 @@
  */
 package exercicios;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author João Pablo
@@ -15,7 +18,41 @@ public class parte3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int chute = 0,
+                sorteado,
+                tentativas = 1;
+
+        Scanner input = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        sorteado = rand.nextInt(100) + 1;
+        System.out.println("Adivinhe qual o Número entre 1 e 100");
+        System.out.println(sorteado);
+
+        do {
+            System.out.println("Número de tentativas: " + tentativas);
+
+            System.out.println("Qual seu palpite: ");
+            chute = input.nextInt();
+
+            tentativas++;
+            if (chute == sorteado + 1 || chute == sorteado - 1) {
+                System.out.println("TÁ QUENTE!!!");
+            } else {
+                if (chute != sorteado && chute > sorteado) {
+                    System.out.println("O seu chute é maior que o número");
+                } else if (chute != sorteado && chute < sorteado) {
+                    System.out.println("O seu chute é menor que o número");
+                }
+            }
+        } while (chute != sorteado && tentativas < 6);
+        if (tentativas == 6) {
+            System.out.println("GAME OVER!!!");
+            System.out.println("O número sorteado era: " + sorteado);
+        } else {
+            System.out.println("Parabéns, você ganhou o jogo!");
+        }
     }
     
 }
